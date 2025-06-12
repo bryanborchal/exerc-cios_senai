8b99616600d2e5f527c2795e85a4f8240e3768a4
bryanborchal <bryanborchal597@gmail.com>
2025-06-12 11:50:15 -0300

    Add files via upload

--- Gerador_De_Username.py
+++ Gerador_De_Username.py
@@ -1,10 +1,13 @@
+#|------------------------------ JEITO MENOS PRÁTICO ------------------------------|
 nome = input('Digite seu nome completo: ')
-nome = nome.strip(" ")
+nome = nome.strip()
 nome = nome.lower()
-nome = nome.replace(' ', '.')
+nome = nome.split()
+primeiro_nome = (nome[0])
+ultimo_nome = nome[-1]
+print(primeiro_nome+"."+ultimo_nome)
 
-#print(type(mensagem))
-#mensagem = mensagem.split()
-#print(mensagem)
-#print(type(mensagem))
-#print(mensagem[0])
+#|------------------------------ JEITO MAIS PRÁTICO ------------------------------|
+
+nome = input('Digite seu nome completo: ').lower().strip().split()
+print(f'{nome[0]}.{nome[-1]}')
